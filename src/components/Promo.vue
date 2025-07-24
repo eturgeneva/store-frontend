77ca2b0641bd1204582bd6b5232a056a524e06ac
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-07-24 11:25:07 +0200

    add promo banner

--- src/components/Promo.vue
+++ src/components/Promo.vue
@@ -1,9 +1,13 @@
 <script setup>
 
+const productImgURL = 'https://eturgeneva.github.io/toy-store-assets/';
+const promoBanner = productImgURL + "promo-banner-1.png"
+
 </script>
 
 <template>
-    <div class="promoBanner">
-        Promo banner
+    <div class="promoSection">
+        <!-- Promo banner -->
+        <img class="promoBanner" :src="promoBanner">
     </div>
 </template>
