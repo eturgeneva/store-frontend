f46fd347c0744b261f9789049b913daf04e1ef03
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-07-25 10:53:14 +0200

    add constructor to store-api class and cleanup getCart method

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 import router from './router';
 import StoreApi from './store-api';
 
+const URL = 'http://localhost:3000';
+
 const app = createApp(App);
 
-app.config.globalProperties.$api = new StoreApi();
+app.config.globalProperties.$api = new StoreApi(URL);
 
 app.use(router).mount('#app');
 
--- src/store-api.js
+++ src/store-api.js
@@ -1,16 +1,14 @@
 export default class StoreApi {
-    // constructor() {
-
-    // }
+    constructor(url) {
+        this.url = url;
+    }
 
     // Cart
     async getCart() {
-        // store.setCartIsLoading(true);
-        try {
-            // console.log('Store cart property:', store.cart.products);
-            console.log('Fetching cart');
+        console.log('Fetching cart');
 
-            const userResponse = await fetch('http://localhost:3000/users/me', {
+        try {
+            const userResponse = await fetch(`${this.url}/users/me`, {
                 credentials: 'include'
             });
             const user = await userResponse.json();
@@ -19,26 +17,19 @@ export default class StoreApi {
 
             if (!cartId) {
                 console.warn('No cart ID found for user');
-                // store.setCartIsLoading(false);
-                // return;
                 return false;
             }
             
-            const cartResponse = await fetch(`http://localhost:3000/carts/${cartId}`, {
+            const cartResponse = await fetch(`${this.url}/carts/${cartId}`, {
                 credentials: 'include'
             })
             if (cartResponse.ok) {
                 const cart = await cartResponse.json();
                 console.log('Cart', cart);
                 return cart;
-                // store.setCart(cart);
-                // console.log('Store cart property:', store.cart.products);
-                // store.setCartIsLoading(false);
             }
-            
         } catch (err) {
             console.error(err);
-            // store.setCartIsLoading(false);
             return false;
         }
     }
