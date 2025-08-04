c9ddfa7a7a50f28fb9a53f0f3fd36a1f6f257ef4
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-08-04 18:32:58 +0200

    change arrow background

--- src/components/ProductsPreviews.vue
+++ src/components/ProductsPreviews.vue
@@ -218,13 +218,15 @@ async function addToCart(productId, quantity) {
     top: 50%;
     transform: translateY(-50%);
     width: 3rem;
-    height: 3rem;
-    border-radius: 50%;
+    height: 4rem;
+    /* border-radius: 50%; */
+    border-radius: 10px;
     box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
     z-index: 2;
     transition: all 0.3s ease;
     backdrop-filter: blur(10px);
     margin: 0;
+    background: white;
 }
 
 .scrollArrow:hover {
