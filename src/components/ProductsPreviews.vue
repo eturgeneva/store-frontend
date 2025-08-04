d972ce01922c8720440fa25a15c41119ffc8e1db
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-08-04 15:26:40 +0200

    wip ProductsPreviews component

--- src/components/ProductsPreviews.vue
+++ src/components/ProductsPreviews.vue
@@ -1,4 +1,63 @@
 <script setup>
+import { ref, onBeforeMount, getCurrentInstance } from 'vue';
+import { store } from '@/store';
+
+const { appContext } = getCurrentInstance();
+const $api = appContext.config.globalProperties.$api;
+
+const productImgURL = 'https://eturgeneva.github.io/toy-store-assets/';
+
+const products = ref([]);
+
+onBeforeMount(async () => {
+    try {
+        const fetchedProducts = await $api.getAllProducts();
+        if (fetchedProducts) {
+            // products.value = fetchedProducts;
+            // Display first 8 products
+            products.value = fetchedProducts.slice(0, 8);
+            console.log('Products Previews', products.value);
+        } else {
+            console.log('Failed to fetch products');
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+async function addToCart(productId, quantity) {
+    console.log('Add to cart store cart ID', store.cartId);
+
+    try {
+        // If a new cart needs to be created
+        if (!store.cartId) {
+            const newCartId = await $api.createCart();
+            console.log('new cart ID', newCartId);
+
+            if (newCartId) {
+                store.setCartId(newCartId);
+                const newCart = await $api.getCart(newCartId);
+                store.setCart(newCart);
+                console.log('Newly created cart', store.cart.products);
+                console.log('New store cart ID', store.cartId);
+            } else {
+                console.log('Failed to create cart');
+            }
+        }
+        // If a cart already exists, but needs to be updated
+        const cartUpdate = await $api.updateQuantityInCart(store.cartId, productId, quantity);
+        if (cartUpdate) {
+            store.setCart(cartUpdate);
+            console.log('Updated cart', store.cart.products);
+            console.log('Updated cart ID', store.cartId);
+
+        } else {
+            console.log('Failed to update cart');
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 </script>
 
