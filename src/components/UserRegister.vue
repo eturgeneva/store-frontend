99cc3a7f2ce566fa92985a3e2b7f9886a3db855a
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-08-06 13:10:55 +0200

    update elements rendered by UserRegister

--- src/components/UserRegister.vue
+++ src/components/UserRegister.vue
@@ -92,29 +92,63 @@ async function registerUser({ email, password, passwordConfirm, firstName, lastN
 </script>
 
 <template>
-    <div class="userRegister framed">
-            <label for="email">Email</label>
-            <input v-model="email" type="text" name="email" id="email">
+    <!-- <div class="userRegister framed"> -->
+    <div class="registerCard">
+        <div class="cardHeader">
+            <h2>Create an Account</h2>
+            <p>Join our community today</p>
+        </div>
 
-            <label for="password">Password</label>
-            <input v-model="password" type="password" name="password" id="password">
-
-            <label for="password">Confirm Password</label>
-            <input v-model="passwordConfirm" type="password" name="passwordConfirm" id="passwordConfirm">
-            
-            <label for="first_name">First Name</label>
-            <input v-model="firstName" type="text" name="first_name" id="first_name"></input>
-
-            <label for="last_name">Last Name</label>
-            <input v-model="lastName" type="text" name="last_name" id="last_name"></input>
-
-            <label for="username">Username</label>
-            <input v-model="username" type="text" name="username" id="username"></input>
-
-            <div class="buttons">
-                <button @click="registerUser({ email, password, passwordConfirm, firstName, lastName, username })" 
-                class="registerButton">Register</button>
+        <form @submit.prevent="registerUser" class="registerForm">
+            <div>
+                <label for="first_name" class="formLabel">First Name</label>
+                <input v-model="firstName" type="text" name="first_name" id="first_name"
+                        class="formInput" placeholder="Enter your first name" required>
             </div>
+
+            <div>
+                <label for="last_name" class="formLabel">Last Name</label>
+                <input v-model="lastName" type="text" name="last_name" id="last_name"
+                        class="formInput" placeholder="Enter your last name" required>
+            </div>
+
+            <div>
+                <label for="username" class="formLabel">Username</label>
+                <input v-model="username" type="text" name="username" id="username"
+                        class="formInput" placeholder="Choose a username" required>
+            </div>
+
+            <div>
+                <label for="email" class="formLabel">Email</label>
+                <input v-model="email" type="text" name="email" id="email"
+                        class="formInput" placeholder="Enter your email" required>
+            </div>
+
+            <div>
+                <label for="password" class="formLabel">Password</label>
+                <input v-model="password" type="password" name="password" id="password"
+                        class="formInput" placeholder="Create a password" required>
+            </div>
+
+            <div>
+                <label for="password" class="formLabel">Confirm Password</label>
+                <input v-model="passwordConfirm" type="password" name="passwordConfirm" id="passwordConfirm"
+                        class="formInput" placeholder="Confirm your password" required>
+            </div>
+
+            <div v-if="errorMessage" class="errorMessage">
+                {{ errorMessage }}
+            </div>
+
+            <div v-if="successMessage" class="successMessage">
+                {{ successMessage }}
+            </div>
+
+            <div class="formActions">
+                    <button @click="registerUser({ email, password, passwordConfirm, firstName, lastName, username })" 
+                    class="registerButton">Register</button>
+            </div>
+        </form>
     </div>
 </template>
 
