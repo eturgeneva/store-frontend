472936d7068115e6dcb204a78315736c552a71d3
elena <elena.turgeneva@gmail.com>
2025-11-12 15:57:19 +0100

    update wishlist rendering inside wishlist component

--- src/components/Wishlist.vue
+++ src/components/Wishlist.vue
@@ -8,13 +8,27 @@ const $api = appContext.config.globalProperties.$api;
 const wishlist = ref([]);
 console.log('wishlist', wishlist)
 console.log('wishlist.value', wishlist.value)
+console.log('store logged in user wishlist', store.loggedInUser.wishlist)
 
+// onBeforeMount(async () => {
+//     try {
+//         const wishlist = await $api.getWishlist();
+//         if (wishlist) {
+//             wishlist.value = wishlist;
+//             console.log('wishlist', wishlist.value);
+//         } else {
+//             console.log('Failed to fetch wishlist');
+//         }
+//     } catch (err) {
+//         console.error(err);
+//     }
+// })
 onBeforeMount(async () => {
     try {
         const wishlist = await $api.getWishlist();
         if (wishlist) {
-            wishlist.value = wishlist;
-            console.log('wishlist', wishlist.value);
+            store.loggedInUser.wishlist = wishlist;
+            console.log('wishlist', store.loggedInUser.wishlist);
         } else {
             console.log('Failed to fetch wishlist');
         }
