a04d5bd81cf4fb61fadb4a7e47d72ced42b4263d
Elena Turgeneva <elena.turgeneva@gmail.com>
2026-04-29 14:53:01 +0200

    update orders fetching

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -6,8 +6,42 @@ const { appContext } = getCurrentInstance();
 const $api = appContext.config.globalProperties.$api;
 
 onBeforeMount(async () => {
+    await getProfile();
     await loadOrders();
 });
+
+async function getProfile() {
+    console.log('store loggedIn', store.loggedIn);
+    console.log('store loggedInUser', store.loggedInUser);
+    try {
+        const user = await $api.getUser();
+        if (user) {
+            console.log('User object', user);
+            console.log('User first name:', user.first_name);
+            console.log('User cart ID:', user.cartId);
+
+            store.setLoggedIn(user.id !== null);
+            if (user.id !== null) {
+                Object.assign(store.loggedInUser, user);
+            }
+            console.log('Store loggedIn', store.loggedIn);
+
+            let cartId = user.cartId;
+            store.setCartId(cartId);
+
+            // If a cart exists
+            if (cartId) {
+                store.setCartIsLoading(true);
+                const cart = await $api.getCart(cartId);
+                store.setCart(cart);
+                console.log('Store cart property:', store.cart.products);
+            } 
+            store.setCartIsLoading(false);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 // Get orders by user:
 async function loadOrders() {
@@ -39,6 +73,7 @@ async function loadOrders() {
             <div>Items: {{  order.product_count }}</div>
             <div>Status: {{ order.status }}</div>
             <div>Total price: {{ order.total_price }}</div>
+            <div>Placed: {{ order.placed_at }}</div>
 
             <router-link :to="`/orders/${order.id}`">Show details</router-link>
         </div>
