b5cbe6257b50cc029b725c5a2a16f596d3603496
Elena Turgeneva <elena.turgeneva@gmail.com>
2026-05-12 14:18:23 +0200

    create item component

---
+++ src/components/Item.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const { appContext } = getCurrentInstance();
+const $api = appContext.config.globalProperties.$api;
+
+const productImgURL = 'https://eturgeneva.github.io/toy-store-assets/';
+
+</script>
+
+<template>
+    <div class="item">
+        <img class="itemImage">
+        <div class="itemInfo">
+            <div class="itemName"></div>
+            <div class="itemBrand"></div>
+            <div class="itemQuantity"></div>
+            <div class="itemPrice"></div>
+        </div>
+        <div class="itemSummary">
+            <div></div>
+        </div>
+
+    </div>
+</template>
