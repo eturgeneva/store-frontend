0f770ee991f459dea668066253f253e6ecdc8b51
Elena Turgeneva <elena.turgeneva@gmail.com>
2026-07-08 14:24:34 +0200

    update layout and css styling of the Promo component

--- src/components/Promo.vue
+++ src/components/Promo.vue
@@ -6,13 +6,26 @@ const productImgURL = 'https://eturgeneva.github.io/toy-store-assets/';
 
 <template>
     <div class="promoSection">
-        <div class="promoBanner">
-            <div class="bannerOverlay"></div>
+        <!-- <div class="promoBanner"> -->
+            <!-- <div class="bannerOverlay"></div> -->
+            <div class="promoMedia">
+                <img
+                    :src="`${productImgURL}promo-banner-2.png`"
+                    alt="Toy collection for promotion"
+                />
+            </div>
 
             <div class="promoText">
-                <!-- <h2>Buy them all!</h2> -->
+                <h1 id="promoTitle">
+                    Bright toys.
+                    <br />
+                    Big little moments.
+                </h1>
+                <p>Discover cheerful picks made for gifting, collecting, and everyday play.</p>
+                <a class="promoButton" href="#products">Explore the collection</a>
             </div>
-        </div>
+            <p class="promoCaption">Soft toys · Collectibles · Made for joy</p>
+        <!-- </div> -->
     </div>
 </template>
 
